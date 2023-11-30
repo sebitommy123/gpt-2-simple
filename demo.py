68bb31ba4467c92a5a2ab14e61b2f155d4e0dbc5
Sebastiaan David Szafir <sebi.szafir@hotmail.com>
2023-11-30 15:00:18 -0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -8,12 +8,13 @@ if not os.path.isdir(os.path.join("models", model_name)):
 	gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
 
 
-file_name = "all_jokes.txt"
+file_name_training = "all_jokes.txt"
+file_name_verification = "all_jokes_validation.txt"
 
 sess = gpt2.start_tf_sess()
 gpt2.finetune(sess,
-              file_name,
-              file_name,
+              file_name_training,
+              file_name_verification,
               model_name=model_name,
               validate_every=100,
               experiment_name="finetune_upper", # Make sure to change if you re-run so it won't override.
